002c6cfe044cdd18ee39830a1002e4f0f513399e
Camila Marrugo <marrugocamille@gmail.com>
2026-03-10 16:05:23 -0500

    Actualizado

--- inventario.py
+++ inventario.py
@@ -1,19 +1,19 @@
 from productos import *
-from history import * #importamos todas las funciones de los otros archivos
+from history import *
 
-print("-" * 34) #decoración repite el guión medio - 34 veces
+print("-" * 34)
 print("Welcome to the ABC Inventory".center(33))
 print("-" * 34)
 running = True #varibale con valor booleano para no usar while true (︺︹︺)
 
-while running: #mientras running sea verdadero se va a ejecutar.
+while running:
 
-    new_product() #función para agregar producto. Formulario para ingresar prod. el diccionario y el print del actual producto.
-    exit = input("Do you wanna exit? (Y/N): ").lower() #la variable para el break del while.
+    new_product()
+    exit = input("Do you wanna exit? (Y/N): ").lower()
 
-    if exit == "y": #si la variable es y de yes muestra el resumen/historial y total para luego definir running como false para romper el bucle.
+    if exit == "y":
         history()
-        print("Have a nice rest of the day!")
+        print("\nHave a nice rest of the day!")
         print("Exiting...")
         running = False 
 
--- productos.py
+++ productos.py
@@ -2,26 +2,20 @@ products = []
 
 def new_product():
     name = input("Enter product name: ")
-    price = int(input("Enter the unit price of the product: ").replace(",","").replace(".","")) #lo puse int pq al ponerlo en float y el usuario 
-    #digitar un precio con un . haciendo entender que es un mil lo va a convertir en decimal ej: 2.000 = 2.0, lo puse entero y para que no haya 
-    #errores reemplacé las , y . en caso de que llegue a digitarlo a nada osea "" vacio las elimino antes de convertir el numero
+    price = int(input("Enter the unit price of the product: ").replace(",","").replace(".",""))
     amount = int(input("Enter quantity: "))
-    total = price * amount #multiplico el precio por cantida
+    total = price * amount
 
-    if price == "":
-        print("Error: you must enter a number.")
-        return
-
-    new_products = { #diccionario donde estoy guardando la info
+    new_products = { 
         "name" : name,
         "price" : price,
         "amount" : amount,
         "total" : total
     }
 
-    products.append(new_products) #aqui las estoy enviando a la lista
+    products.append(new_products)
 
-    print("=" * 34) #aqui imprimo el producto actual
+    print("=" * 34)
     print(f"Name: {name:>27}")
     print(f"Price: ${price:>25,.0f}")
     print(f"Amount: {amount:>25}")
@@ -29,7 +23,7 @@ def new_product():
     print("=" * 34)
 
 
-def show_product(): #muestro el historial, un for para que recorra toda la lista y la muestre
+def show_product():
     if not products:
         print("There's not products")
     else :
