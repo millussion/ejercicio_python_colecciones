0675aa00d3218ea38c5d72670fb17fbb5a1e8613
Camille Marrugo <marrugocamille@gmail.com>
2026-03-11 07:41:51 -0500

    Update history.py
    Added comments to clarify the purpose of the code.

--- history.py
+++ history.py
@@ -2,10 +2,10 @@ from products_all import *
 
 
 def history():
-    total_day = sum(product["total"] for product in products)
+    total_day = sum(product["total"] for product in products) #with the function of python sum im adding all the totals of every product
     print("-" * 34)
-    print("Sales history of the day".center(35))
-    show_product()
+    print("Sales history of the day".center(35)) #center is for center
+    show_product() #here i'm showing the function
     print(f"Total Day: ${total_day:>21,.0f}")
 
     
