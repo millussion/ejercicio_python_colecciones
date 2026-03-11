4c19d80ab6d4f8c95d625de28b3734e5275505b1
Camille Marrugo <marrugocamille@gmail.com>
2026-03-11 07:44:21 -0500

    Update products_all.py

--- products_all.py
+++ products_all.py
@@ -2,20 +2,22 @@ products = []
 
 def new_product():
     name = input("Enter product name: ")
-    price = int(input("Enter the unit price of the product: ").replace(",","").replace(".",""))
-    amount = int(input("Enter quantity: "))
+    price = int(input("Enter the unit price of the product: ").replace(",","").replace(".","")) #I used int because if I put it in float and the user
+    #enters a price with a . (meaning it's a thousand), it will be converted to a decimal, e.g., 2.000 = 2.0. I used integer and to avoid
+    #errors, I replaced the , and . (in case they enter nothing, i.e., an empty space), I remove them before converting the number
+    amount = int(input("Enter quantity: ")) #multiply the price by the quantity
     total = price * amount
 
-    new_products = { 
+    new_products = { #dictionary where I'm saving the info
         "name" : name,
         "price" : price,
         "amount" : amount,
         "total" : total
     }
 
-    products.append(new_products)
+    products.append(new_products) #Here I am sending them to the list
 
-    print("=" * 34)
+    print("=" * 34) #Here I print the CURRET product
     print(f"Name: {name:>27}")
     print(f"Price: ${price:>25,.0f}")
     print(f"Amount: {amount:>25}")
@@ -23,12 +25,12 @@ def new_product():
     print("=" * 34)
 
 
-def show_product():
+def show_product(): #I display the history, a for loop to iterate through the entire list and display it
     if not products:
         print("There's not products")
     else :
 
-        for product in products:
+        for product in products: 
             print("=" * 34)
             print(f"Name: {product['name']:>27}"),
             print(f"Price: ${product['price']:>25,.0f}"),
