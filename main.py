e0898dc947f3b36745f0566a7937c875d04bccbb
Camille Marrugo <marrugocamille@gmail.com>
2026-03-11 07:42:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from products_all import *
-from history import *
+from history import * #we import all the functions from the other files
 
-print("-" * 34)
+print("-" * 34) 
 print("Welcome to the ABC Inventory".center(33))
 print("-" * 34)
-running = True #varibale con valor booleano para no usar while true (︺︹︺)
+running = True #variable with boolean value to avoid using while true (︺︹︺)
 
 while running:
 
-    new_product()
-    exit = input("Do you wanna exit? (Y for EXIT, any letter to continue): ").lower()
+    new_product() #function to add product. Form to enter product, dictionary and print current product.
+    exit = input("Do you wanna exit? (Y for EXIT, any letter to continue): ").lower() #the variable for the break of the while loop.
 
-    if exit == "y":
+    if exit == "y": #if the variable is yes, show the summary/history and total, then set running to false to break the loop.
         history()
         print("\nHave a nice rest of the day!")
         print("Exiting...")
